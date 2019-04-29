601b9018dc5e9250b6a07036ece80a7dd7508ef8
Kaylah Malillos <malillosk@gmail.com>
2019-04-29 15:37:44 -0600

    added userGuess and computerGuess variables in js file

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,18 @@
 var computerChoices = ["r", "p", "s"]
+
+document.onkeyup = function(event) {
+    // console.log(event.key);
+
+    var userGuess = event.key;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+    
+    console.log("User Guess: ", userGuess);
+    console.log("Computer Guess: ", computerGuess);
+
+    if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
+        console.log("Valid guess!")
+    } else {
+        alert("Please press 'r', 'p', or 's' to play the game.")
+    }
+
+}
