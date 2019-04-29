8c53e9798764696ce9a41471d70b31ca605f4667
Kaylah Malillos <malillosk@gmail.com>
2019-04-29 15:58:32 -0600

    added logic to game

--- assets/game.js
+++ assets/game.js
@@ -1,16 +1,34 @@
-var computerChoices = ["r", "p", "s"]
+var gameOptions = ["r", "p", "s"]
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
 document.onkeyup = function(event) {
     // console.log(event.key);
 
-    var userGuess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+    var userChoice = event.key.toLowerCase();
+    var computerChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)]
     
-    console.log("User Guess: ", userGuess);
-    console.log("Computer Guess: ", computerGuess);
+    // console.log("User Choice: ", userChoice);
+    // console.log("Computer Choice: ", computerChoice);
 
-    if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
-        console.log("Valid guess!")
+    if (userChoice === "r" || userChoice === "p" || userChoice === "s") {
+        // console.log("Valid Choice!")
+        if (userChoice === "r" && computerChoice === "s" ) {
+            wins++;
+        } else if (userChoice === "p" && computerChoice === "r" ) {
+            wins++;
+        } else if (userChoice === "s" && computerChoice === "p") {
+            wins++;
+        } else if (userChoice === "r" && computerChoice === "p") {
+            losses++;
+        } else if (userChoice === "p" && computerChoice === "s") {
+            losses++;
+        } else if (userChoice === "s" && computerChoice === "r") {
+            losses++;
+        } else {
+            tie++;
+        };
     } else {
         alert("Please press 'r', 'p', or 's' to play the game.")
     }
